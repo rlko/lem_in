48aeb4c98de7ad126a627cee5626e1417ff2ca3e
Ronald LIOU-KEE-ON <rliou-ke@e1r10p9.42.fr>
2016-04-27 19:01:20 +0200

    Todo: coords (int) parse

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: rliou-ke <rliou-ke@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/11 10:05:35 by rliou-ke          #+#    #+#             */
-/*   Updated: 2016/04/27 16:26:27 by rliou-ke         ###   ########.fr       */
+/*   Updated: 2016/04/27 18:57:47 by rliou-ke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,72 +67,111 @@ void		assign_coord(char *str, int *cd)
 
 void		assign_room(t_list *lst, t_dome **room)
 {
-	int		i;
 	t_list	*tmp;
 
-	if ((i = ft_lstlen(lst)) != 3)
-		ft_exit_error("ERROR");
-	i = 0;
-	while (lst != NULL)
+	(*room)->name = lst->content;
+	lst = lst->next;
+	(*room)->x = ft_atoi(lst->content);
+	(*room)->y = ft_atoi(lst->next->content);
+
+	while (lst)
 	{
 		tmp = lst;
-		if (i == 0)
-			(*room)->name = lst->content;
-		else
-		{
-			if (i == 1)
-				assign_coord(lst->content, &(*room)->x);
-			else if (i == 2)
-				assign_coord(lst->content, &(*room)->y);
-			else
-				ft_exit_error("assign_room: wut");
-		}
 		lst = lst->next;
 		free(tmp);
-		++i;
 	}
 }
 
 void		assign_type(t_dome **room, char *prev)
 {
 	if (ft_strequ("##start", prev))
-	{
 		(*room)->type = STROOM;
-	}
 	else if (ft_strequ("##end", prev))
-	{
 		(*room)->type = EDROOM;
-	}
 	else
-	{
 		(*room)->type = RGROOM;
-	}
 }
 
-t_dome		*mk_rooms(t_dome *head, char **prev, char *line)
+int			data_are_unique(t_dome *head, t_list *lst)
 {
-	t_dome	*tmp;
+	int		x;
+	int		y;
+	t_dome *tmp;
+
+	x = ft_atoi(lst->next->content);
+	y = ft_atoi(lst->next->next->content);
+	tmp = head;
+	while (tmp != NULL)
+	{
+		if (ft_strequ(lst->content, tmp->name))
+			return (0);
+		tmp = tmp->next;
+	}
+	tmp = head;
+	while (tmp != NULL)
+	{
+		if (x == tmp->x && y == tmp->y)
+			return (0);
+		tmp = tmp->next;
+	}
+	return (1);
+}
+
+int			split_parse(char *line, t_list **lst, t_dome *head)
+{
+	int 	nb_node;
+
+	if (!(*lst = ft_lstsplit(line, ' ')))
+		ft_exit_error("mk_rooms: lst: ft_lstsplit");
+	nb_node = ft_lstlen(*lst);
+	if (ft_strchr(line, '-'))
+	{
+		if (nb_node == 1)
+			return (0);
+	}
+	else if (nb_node == 3)
+	{
+		if (data_are_unique(head, *lst))
+			return (1);
+	}
+	ft_exit_error("ERROR");
+	return (-42);
+}
+
+t_dome		*domebuilder(t_dome *head, t_dome *room)
+{
+	t_dome *tmp;
+
+	if (head == NULL)
+		head = room;
+	else
+	{
+		tmp = head;
+		while (tmp->next != NULL)
+			tmp = tmp->next;
+		tmp->next = room;
+	}
+	return (head);
+}
+
+int			mk_rooms(t_dome **head, char **prev, char *line)
+{
 	t_dome	*room;
 	t_list	*lst;
 
 	if (!line[0])
 		ft_exit_error("ERROR");
+	if (!split_parse(line, &lst, *head))
+		return (0);
 	if (!(room = malloc(sizeof(*room))))
 		ft_exit_error("mk_rooms: room: malloc");
-	if (!(lst = ft_lstsplit(line, ' ')))
-		ft_exit_error("mk_rooms: lst: ft_lstsplit");
-	assign_room(lst, &room);
-	assign_type(&room, *prev);
 	room->next = NULL;
+	assign_type(&room, *prev);
+	assign_room(lst, &room);
 	if (*prev)
 		*prev = NULL;
-	if (head == NULL)
-		return (room);
-	tmp = head;
-	while (tmp->next != NULL)
-		tmp = tmp->next;
-	tmp->next = room;
-	return (head);
+	*head = domebuilder(*head, room);
+	return (1);
 }
 
 char		*check_command(t_list *file, char *prev, char *line)
@@ -148,7 +187,7 @@ char		*check_command(t_list *file, char *prev, char *line)
 t_dome		*find_rooms(t_list **file)
 {
 	char 	*line;
-	int		ret[2];
+	int		ret[3];
 	t_dome	*rooms;
 	char	*prev;
 
@@ -161,7 +200,10 @@ t_dome		*find_rooms(t_list **file)
 		if (ret[1])
 			prev = check_command(*file, prev, line);
 		if (!str_iscomment(line) && !str_iscommand(line, 1))
-			rooms = mk_rooms(rooms, &prev, line);
+		{
+			if (!(ret[2] = mk_rooms(&rooms, &prev, line)))
+				break ;
+		}
 		if ((ret[1] = is_unique(line)) > 1)
 			ft_exit_error("ERROR");
 	}
@@ -180,5 +222,32 @@ int			main(void)
 	if (!(anb = find_antsnbr(&file)))
 		return (ft_error("ERROR"));
 	room = find_rooms(&file);
+	
+	ft_putchar('\n');
+	ft_putendl("Parsing done:");
+
+	int i = 0;
+	while (room)
+	{
+		ft_putstr("Salle: ");
+		ft_putnbr(i++);
+		ft_putchar('\n');
+		ft_putstr("Nom: ");
+		ft_putendl(room->name);
+		ft_putstr("x: ");
+		ft_putnbr(room->x);
+		ft_putchar('\n');
+		ft_putstr("y: ");
+		ft_putnbr(room->y);
+		ft_putchar('\n');
+		room = room->next;
+	}
+	ft_putchar('\n');
+	ft_putendl("Fichier:");
+	while (file)
+	{
+		ft_putendl(file->content);
+		file = file->next;
+	}
 	return (0);
 }
